ccb1a80f360cbaab606716fcac59e4bc23393288
Stefanius <sgrootveld@gmail.com>
2015-12-30 23:12:48 +0100

    Add monthdata to 2016.

--- src/Stefanius/NationalHolidayBundle/Controller/DefaultController.php
+++ src/Stefanius/NationalHolidayBundle/Controller/DefaultController.php
@@ -9,6 +9,21 @@ use Symfony\Component\HttpFoundation\Request;
 
 class DefaultController extends Controller
 {
+    protected $months = [
+        'januari'   => 1,
+        'februari'  => 2,
+        'maart'     => 3,
+        'april'     => 4,
+        'mei'       => 5,
+        'juni'      => 6,
+        'juli'      => 7,
+        'augustus'  => 8,
+        'september' => 9,
+        'oktober'   => 10,
+        'november'  => 11,
+        'december'  => 12,
+    ];
+
     protected function buildPage(array $data = [])
     {
         $page = new Page();
@@ -21,7 +36,6 @@ class DefaultController extends Controller
         }
 
         return $page;
-
     }
 
     protected function setPageTitle(Page $page, array $data = [])
@@ -81,6 +95,32 @@ class DefaultController extends Controller
         return $this->render('StefaniusNationalHolidayBundle:Default:list.html.twig', [
             'dates' => $dates,
             'page'  => $page,
+        ]);
+    }
+
+    public function monthAction(Request $request, $dutchMonthName)
+    {
+        if (!array_key_exists($dutchMonthName, $this->months)) {
+            $this->redirect($this->generateUrl('stefanius_national_holiday_list_all'));
+        }
+
+        $year = $this->getDomainYear($request);
+
+        $parser = new Parser();
+        $dates = $parser->findSpecialDateByMonthNumber($year, $this->months[$dutchMonthName]);
+
+        $page = $this->buildPage([
+            'title'       => 'Bijzondere dagen ' . $dutchMonthName. ' '. $year,
+            'description' => ucfirst($dutchMonthName) . ' ' . $year . ' heeft maar liefst ' . count($dates) . ' dagen om bij stil te staan. Bekijk het hele overzicht van ' . $dutchMonthName,
+            'request'     => $request,
+        ]);
+
+        return $this->render('StefaniusNationalHolidayBundle:Default:bymonth.html.twig', [
+            'dates'          => $dates,
+            'page'           => $page,
+            'dutchMonthName' => $dutchMonthName,
+            'monthNumber'    => $this->months[$dutchMonthName],
+            'months'         => array_keys($this->months),
         ]);
     }
 }
